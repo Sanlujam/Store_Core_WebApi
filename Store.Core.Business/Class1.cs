aca532d61803277cfc43a5118072e68cd73c670d
Jonatan Mancera <alexander.mancera@hasar.com>
2021-09-04 01:44:34 -0300

    First scheme

---
+++ Store.Core.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Store.Core.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ Store.Core.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Store.Core.Data
+{
+    public class Class1
+    {
+    }
+}
